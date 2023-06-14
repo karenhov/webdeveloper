82577089cb2aaa11a526a974a1bcc684eb319ebd
kraklin <karenhov034044@gmail.com>
2023-06-14 08:29:33 +0400

    commit

--- uber/src/js/script.js
+++ uber/src/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
-    menu_items = document.querySelectorAll('.menu_items'),
+    menuItems = document.querySelectorAll('.menuItems'),
     homburger = document.querySelector('.homburger');
 
     homburger.addEventListener('click', () => {
@@ -8,8 +8,8 @@ window.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('menu_active');
     });
 
-    menu_items.forEach(items => {
-        menu_items.addEventListener('click', () => {
+    menuItems.forEach(items => {
+        items.addEventListener('click', () => {
             homburger.classList.toggle('homburger_active');
             menu.classList.toggle('menu_active');
         })
